3536b4a790892c61f0fc2a3c5196609482466462
Steve <stevechikwiri@gmail.com>
2020-12-17 03:48:46 +0200

    added firebase

--- app/src/main/java/com/alcor/music/ui/itemdetails/ItemDetailsFragment.java
+++ app/src/main/java/com/alcor/music/ui/itemdetails/ItemDetailsFragment.java
@@ -1,13 +1,14 @@
 package com.alcor.music.ui.itemdetails;
 
-import android.content.Intent;
 import android.os.Bundle;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 
+import com.alcor.music.R;
+import com.alcor.music.databinding.ActivityCheckoutBinding;
 import com.alcor.music.databinding.FragmentItemDetailsBinding;
-import com.alcor.music.ui.CheckoutActivity;
+import com.google.android.material.bottomsheet.BottomSheetDialog;
 
 import androidx.annotation.NonNull;
 import androidx.fragment.app.Fragment;
@@ -20,13 +21,21 @@ public class ItemDetailsFragment extends Fragment {
                              ViewGroup container, Bundle savedInstanceState) {
         binding = FragmentItemDetailsBinding.inflate(inflater);
 
-        binding.buy.setOnClickListener(v -> startActivity(new Intent(getContext(), CheckoutActivity.class)));
+        binding.buy.setOnClickListener(v -> createTXNEditDialog().show());
 
         return binding.getRoot();
     }
 
-    public void setUpViews(){
+    public BottomSheetDialog createTXNEditDialog(){
+        BottomSheetDialog dialog = new BottomSheetDialog(getContext());
+        LayoutInflater inflater = LayoutInflater.from(getContext());
+        View dialogView = inflater.inflate(R.layout.activity_checkout,null);
+        ActivityCheckoutBinding dialogBinding = ActivityCheckoutBinding.bind(dialogView);
+        dialogBinding.title.setText("The Book of Eli");
 
+        dialog.setContentView(dialogView);
+
+        return dialog;
     }
 
 }
